c6a11ebbc35c43c8b8a4c3323912ff93c8fa8e46
Arlene Gustafson <arlene@gustafsonfamily.org>
2020-02-21 16:02:32 -0700

    Finish todo to display venue data at endpoint /venues/<id>

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -158,86 +158,60 @@ def search_venues():
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
-  # TODO: replace with real venue data from the venues table, using venue_id
-  data1={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-    "past_shows": [{
-      "artist_id": 4,
-      "artist_name": "Guns N Petals",
-      "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-      "start_time": "2019-05-21T21:30:00.000Z"
-    }],
-    "upcoming_shows": [],
-    "past_shows_count": 1,
-    "upcoming_shows_count": 0,
-  }
-  data2={
-    "id": 2,
-    "name": "The Dueling Pianos Bar",
-    "genres": ["Classical", "R&B", "Hip-Hop"],
-    "address": "335 Delancey Street",
-    "city": "New York",
-    "state": "NY",
-    "phone": "914-003-1132",
-    "website": "https://www.theduelingpianos.com",
-    "facebook_link": "https://www.facebook.com/theduelingpianos",
-    "seeking_talent": False,
-    "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-    "past_shows": [],
-    "upcoming_shows": [],
-    "past_shows_count": 0,
-    "upcoming_shows_count": 0,
-  }
-  data3={
-    "id": 3,
-    "name": "Park Square Live Music & Coffee",
-    "genres": ["Rock n Roll", "Jazz", "Classical", "Folk"],
-    "address": "34 Whiskey Moore Ave",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "415-000-1234",
-    "website": "https://www.parksquarelivemusicandcoffee.com",
-    "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
-    "seeking_talent": False,
-    "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-    "past_shows": [{
-      "artist_id": 5,
-      "artist_name": "Matt Quevedo",
-      "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-      "start_time": "2019-06-15T23:00:00.000Z"
-    }],
-    "upcoming_shows": [{
-      "artist_id": 6,
-      "artist_name": "The Wild Sax Band",
-      "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-      "start_time": "2035-04-01T20:00:00.000Z"
-    }, {
-      "artist_id": 6,
-      "artist_name": "The Wild Sax Band",
-      "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-      "start_time": "2035-04-08T20:00:00.000Z"
-    }, {
-      "artist_id": 6,
-      "artist_name": "The Wild Sax Band",
-      "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-      "start_time": "2035-04-15T20:00:00.000Z"
-    }],
-    "past_shows_count": 1,
-    "upcoming_shows_count": 1,
-  }
-  data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
-  return render_template('pages/show_venue.html', venue=data)
+  # Done: replace with real venue data from the venues table, using venue_id
+  try:
+    print("venuid=", venue_id)
+    data = []
+    past_shows = []
+    upcoming_shows = []
+    venue = Venue.query.get(venue_id)
+
+    if venue:
+      #get all the shows for this venue, shows is an array of Show objects
+      shows = venue.show
+      for show in shows:    
+        past_artist = Artist.query.filter(Artist.id == show.artist_id, show.start_time < datetime.utcnow()).first()
+        upcoming_artist = Artist.query.filter(Artist.id == show.artist_id, show.start_time > datetime.utcnow()).first()
+        artist = past_artist if past_artist != None else upcoming_artist
+        show_detail = {
+          "artist_id": artist.id,
+          "artist_name": artist.name,
+          "artist_image_link": artist.image_link,
+          "start_time": show.start_time.strftime("%Y-%m-%d %H:%M:%S.%f"),
+        }
+        if past_artist != None:
+          past_shows.append(show_detail)
+        else:
+          upcoming_shows.append(show_detail)
+        
+      recordObject = {
+        "id": venue.id,
+        "name": venue.name,
+        "genres": venue.genres,
+        "address": venue.address,
+        "city": venue.city,
+        "state": venue.state,
+        "phone": venue.phone,
+        "website": venue.website,
+        "facebook_link": venue.facebook_link,
+        "seeking_talent": venue.seeking_talent,
+        "image_link": venue.image_link,
+        "past_shows": past_shows,
+        "upcoming_shows": upcoming_shows,
+        "past_shows_count": Shows.query.filter(
+                  Shows.venue_id == venue.id,
+                  Shows.start_time < datetime.utcnow()).count(),
+        "upcoming_shows_count": Shows.query.filter(
+                  Shows.venue_id == venue.id,
+                  Shows.start_time > datetime.utcnow()).count(),
+      }
+      data.append(recordObject)
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close 
+
+  return render_template('pages/show_venue.html', venue=data[0])
 
 #  Create Venue
 #  ----------------------------------------------------------------
@@ -654,8 +628,6 @@ def populate_database():
           venue_id = 1,
           artist_id = 4
         )
-        print(type(show1.start_time))
-        print(show1.start_time)
 
         show2 = Shows(
           start_time = datetime.strptime("2019-06-15T23:00:00.000Z", '%Y-%m-%dT%H:%M:%S.%fZ'),
@@ -686,7 +658,56 @@ def populate_database():
     db.session.close()
 
 def test_data():
-  None
+  try:
+    data = []
+    past_shows = []
+    upcoming_shows = []
+    venue = Venue.query.get(3)
+
+    if venue:
+      #get all the shows for this venue, shows is an array of Show objects
+      shows = venue.show
+      for show in shows:    
+        past_artist = Artist.query.filter(Artist.id == show.artist_id, show.start_time < datetime.utcnow()).first()
+        upcoming_artist = Artist.query.filter(Artist.id == show.artist_id, show.start_time > datetime.utcnow()).first()
+        artist =past_artist if past_artist != None else upcoming_artist
+        show_detail = {
+          "artist_id": artist.id,
+          "artist_name": artist.name,
+          "artist_image_link": artist.image_link,
+          "start_time": show.start_time,
+        }
+        if past_artist != None:
+          past_shows.append(show_detail)
+        else:
+          upcoming_shows.append(show_detail)
+        
+      recordObject = {
+        "id": venue.id,
+        "name": venue.name,
+        "genres": venue.genres,
+        "address": venue.address,
+        "city": venue.city,
+        "state": venue.state,
+        "phone": venue.phone,
+        "website": venue.website,
+        "facebook_link": venue.facebook_link,
+        "seeking_talent": venue.seeking_talent,
+        "image_link": venue.image_link,
+        "past_shows": past_shows,
+        "upcoming_shows": upcoming_shows,
+        "past_shows_count": Shows.query.filter(
+                  Shows.venue_id == venue.id,
+                  Shows.start_time < datetime.utcnow()).count(),
+        "upcoming_shows_count": Shows.query.filter(
+                  Shows.venue_id == venue.id,
+                  Shows.start_time > datetime.utcnow()).count(),
+      }
+      data.append(recordObject)
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close  
     
 #----------------------------------------------------------------------------#
 # Launch.
